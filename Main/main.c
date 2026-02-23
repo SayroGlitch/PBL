2cb44283e4b9d6f60dea64696825fa0484db2760
dansoftwengineer-sudo <dan.gheorghilas.softw.engineer@gmail.com>
2026-02-23 22:22:30 +0200

    Needed update
    Update la functiile de sortare

--- Main/main.c
+++ Main/main.c
@@ -1,101 +1,124 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <time.h>
-//tim_sort
+
+// TimSort
 #include "Algoritmi/timsort_Bogdan.h"
 #include "Algoritmi/timsort_Bogdan.c"
-//radix_sort
+// Radix Sort
 #include "Algoritmi/radix_sort.c"
 #include "Algoritmi/radix_sort.h"
-//quick_sort(e temporal)
+// Quick Sort (temporar)
 #include "Algoritmi/quick_sort.c"
 #include "Algoritmi/quick_sort.h"
-//selection_sort(e temporal)
+// Selection Sort (temporar)
 #include "Algoritmi/selection_sort.c"
 #include "Algoritmi/selection_sort.h"
-//merge_sort(e temporal)
+// Merge Sort (temporar)
 #include "Algoritmi/merge_sort.c"
 #include "Algoritmi/merge_sort.h"
 
 
-void print_menu() {
-    printf("\nAlgorithms:\n");
+void afiseaza_meniu() {
+    printf("\nSELECT ALGORITM:\n");
     printf("1. Tim Sort\n");
     printf("2. Radix Sort\n");
     printf("3. Quick Sort\n");
     printf("4. Selection Sort\n");
     printf("5. Merge Sort\n");
     printf("0. Quit\n");
-    printf("Choose one: ");
+    printf("You've selected : ");
 }
 
 int main() {
-    //folosim pentru a masura timp
-      
     int optiune, n_elemente, tip_ordine, output_dest;
-    char filename[100], ordine_str[20];
-    
-    while (1) {
-        print_menu();
-        scanf("%d", &optiune);
-        if (optiune == 0) break;
+    char filename[150], ordine_str[20];
+    struct timespec start, end;
 
-        printf("Numar elemente (100, 1000, 10000, 100000, 1000000): ");
+    while (1) {
+        afiseaza_meniu();
+        if (scanf("%d", &optiune) != 1 || optiune == 0) break;
+
+        printf("Insert number of elements for sorting (100, 1000, 10000, 100000, 1000000): ");
         scanf("%d", &n_elemente);
 
-        printf("Tip ordine (1. Ascendent, 2. Descendent, 3. Random): ");
+        printf("From the beginning elements are sorted as (1. ascendent, 2. descendent(worst), 3. random): ");
         scanf("%d", &tip_ordine);
-        
-        // usor alegem ce sa rulam
-        switch(tip_ordine) {
+
+        // alegem sub ce forma sa fie elementele de la inceput(pentru complexitate diferita)
+        switch (tip_ordine) {
             case 1: strcpy(ordine_str, "ascendent"); break;
             case 2: strcpy(ordine_str, "descendent"); break;
             default: strcpy(ordine_str, "random"); break;
         }
+
+        // concatenam calea la fisierul meu
         sprintf(filename, "inputs/input_%d_%s.txt", n_elemente, ordine_str);
 
-        // Citire date din fisier
         FILE *fin = fopen(filename, "r");
         if (!fin) {
-            printf("Eroare: Nu s-a putut deschide fisierul %s\n", filename);
+            printf(" Eroare: We can't find file %s.Check inputs folder \n", filename);
             continue;
         }
 
+        // alocarea dinamica
         int *arr = (int*)malloc(n_elemente * sizeof(int));
+        if (!arr) {
+            printf(" Error: No memory !\n");
+            fclose(fin);
+            continue;
+        }
+
         for (int i = 0; i < n_elemente; i++) {
-            fscanf(fin, "%d", &arr[i]);
+            if (fscanf(fin, "%d", &arr[i]) == EOF) break;
         }
         fclose(fin);
 
-          clock_t start = clock();
-        switch (optiune) {
-            case 1: /* tim_sort(arr, n_elemente); */ break; 
-            case 2: /* radix_sort(arr, n_elemente); */ break;
-            case 3: /* quick_sort(arr, 0, n_elemente - 1); */ break;
-            case 4: /* selection_sort(arr, n_elemente); */ break;
-            case 5: /* merge_sort(arr, 0, n_elemente - 1); */ break;
-        }
-        clock_t end = clock();
-        double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+        printf("We sort:\n");
 
-        // Destinatie Output
-        printf("Where to output? (1.Screen, 2. out.txt): ");
+        //masuram in nanosec
+        timespec_get(&start, TIME_UTC);
+
+        switch (optiune) {
+            case 1: timSort(arr, n_elemente); break; 
+            case 2: radixSort(arr, n_elemente); break;
+            case 3: quickSort(arr, 0, n_elemente - 1); break;
+            case 4: selectionSort(arr, n_elemente); break;
+            case 5: mergeSort(arr, 0, n_elemente - 1); break;
+            default: printf("Invalid variant!\n"); break;
+        }
+
+        //vedem timpul final
+        timespec_get(&end, TIME_UTC);
+
+        // diferenta de secunde
+        long long nanosecunde = (long long)(end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
+
+        printf("Where do you want to save results? (1. Screen, 2. out.txt): ");
         scanf("%d", &output_dest);
 
         if (output_dest == 2) {
             FILE *fout = fopen("out.txt", "a");
-            for (int i = 0; i < n_elemente; i++) fprintf(fout, "%d ", arr[i]);
-            fclose(fout);
-            printf("Saved in out.txt. Time of execution: %f sec.\n", time_taken);
+            if (fout) {
+                for (int i = 0; i < n_elemente; i++) fprintf(fout, "%d ", arr[i]);
+                fclose(fout);
+                printf("Success! Check file out.txt.\n");
+            }
         } else {
+            printf("\nSorted vector is:\n");
             for (int i = 0; i < n_elemente; i++) printf("%d ", arr[i]);
-            printf("\nTime of execution: %f sec.\n", time_taken);
+            printf("\n");
         }
 
-        free(arr);
+        printf("\n>>> Time of execution : %lld nanosec <<<\n", nanosecunde);
+        printf(">>> Approximate: %.6f milisec <<<\n", (double)nanosecunde / 1000000.0);
+
+        free(arr); //eliberam memoria noastra
     }
 
+    printf("\n");
     return 0;
 }
