755eb62d7a7ba858bf5b773d5aecc41091f84668
SayroGlitch <bumbumbam821@gmail.com>
2026-02-23 14:30:35 +0200

    timsort added and working

--- Main/main.c
+++ Main/main.c
@@ -1,7 +1,17 @@
 #include "stdio.h"
+#include "stdlib.h"
+#import "../timsort_Bujdan.c"
+#include "time.h"
 
 int main() {
-    printf("Hello World");
-
+    time_t start,end;
+    start = clock();
+    int N[] = {5,3,9,3,4,1,19,54,21,56,12,453,1212,8,3434,22,43,76,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1,1,1};
+    int n = sizeof(N) / sizeof(N[0]);
+    timSort(N,n);
+    end = clock();
+    double timp = (double)(end-start) / CLOCKS_PER_SEC;
+    for (int i=0;i<n;i++) printf("%d ",N[i]);
+    printf("\nTime:%.99lf",timp);
     return 0;
 }
