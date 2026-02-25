6035dae3796373cf84db37590f935b7d8cf5879c
SayroGlitch <bumbumbam821@gmail.com>
2026-02-25 10:38:04 +0200

    Changed the windows.txt to req.txt for a proper understanding of the requirements
    Added terminal.c which will be the main program for terminal initialization

--- Main/terminal.c
+++ Main/terminal.c
@@ -57,7 +57,7 @@ int standart_case() {
         }
 
         // concatenam calea la fisierul meu
-        sprintf(filename, "../inputs/input_%d_%s.txt", n_elemente, ordine_str);
+        sprintf(filename, "PBL/Main/inputs/input_%d_%s.txt", n_elemente, ordine_str);
 
         FILE *fin = fopen(filename, "r");
         if (!fin) {
@@ -102,7 +102,7 @@ int standart_case() {
         scanf("%d", &output_dest);
 
         if(output_dest == 2) {
-            FILE *fout = fopen("../results/out.txt", "a");
+            FILE *fout = fopen("PBL/Main/results/out.txt", "a");
             if (fout) {
                 for (int i = 0; i < n_elemente; i++) fprintf(fout, "%d ", arr[i]);
                 QueryPerformanceCounter(&end);
@@ -171,7 +171,7 @@ int argument_case(char *arg[]) {
     }
 
     // concatenam calea la fisierul meu
-    sprintf(filename, "../inputs/input_%ld_%s.txt", n_elemente, ordine_str);
+    sprintf(filename, "PBL/Main/inputs/input_%ld_%s.txt", n_elemente, ordine_str);
 
     FILE *fin = fopen(filename, "r");
     if (!fin) {
@@ -212,7 +212,7 @@ int argument_case(char *arg[]) {
     double time = (double)(end.QuadPart - start.QuadPart) / frequency.QuadPart;
 
     if (output_dest == 2) {
-        FILE *fout = fopen("../results/out.txt", "a");
+        FILE *fout = fopen("PBL/Main/results/out.txt", "a");
         if (fout) {
             for (int i = 0; i < n_elemente; i++) fprintf(fout, "%d ", arr[i]);
             QueryPerformanceCounter(&end);
@@ -243,12 +243,12 @@ int main(int argc, char *argv[]) {
     if(argc == 1) standart_case();
     else argument_case(argv);
 
-    FILE *fout = fopen("../results/out.txt", "r");
+    FILE *fout = fopen("PBL/Main/results/out.txt", "r");
     if(fout) {
         printf("\nDo you want to delete the output file?(y/n): ");
 
         scanf(" %c",&delete);
-        if(tolower(delete) == 'y') remove("../results/out.txt");
+        if(tolower(delete) == 'y') remove("PBL/Main/results/out.txt");
     }
     fclose(fout);
     return 0;
